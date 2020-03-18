94c0d97feddbf5eb1993e86033da68e8ed8c12bd
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-18 12:04:43 -0600

    95. Servicio para consumir las noticias de NewsApi.org

---
+++ src/app/services/noticias.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoticiasService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getTopHeadlines() {
+    return this.http.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=545b637c479641509d397ea971a99728`);
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '545b637c479641509d397ea971a99728'
 };
 
 /*
