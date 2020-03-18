f36fdb8db9cc43534e41fa8bf4e0e4ee61d69a63
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-18 12:12:33 -0600

    96. Interfaces para el manejo de la información

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RespuestaTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,8 +12,12 @@ export class NoticiasService {
     private http: HttpClient
   ) { }
 
-  getTopHeadlines() {
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=545b637c479641509d397ea971a99728`);
+  getTopHeadlines(): Observable<RespuestaTopHeadlines> {
+    return this
+            .http
+            .get<RespuestaTopHeadlines>(
+              `http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=545b637c479641509d397ea971a99728`
+            );
   }
 
 }
