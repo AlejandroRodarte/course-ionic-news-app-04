8b695554c4ebbf1cd1f4cc0298fb0a6d51b9e657
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-18 20:06:04 -0600

    101. Mostrar encabezados dependiendo de la categoría

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import { IonSegment } from '@ionic/angular';
 import { NoticiasService } from '../../services/noticias.service';
+import { Article, RespuestaTopHeadlines } from '../../interfaces/interfaces';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-tab2',
@@ -22,13 +24,32 @@ export class Tab2Page implements OnInit {
     'technology'
   ];
 
+  public noticias: Article[] = [];
+
   constructor(
     private noticiasService: NoticiasService
   ) {}
 
   ngOnInit() {
     this.segment.value = this.categorias[0];
-    this.noticiasService.getTopHeadlinesByCategory(this.categorias[0]).subscribe(console.log);
+    this.cargarNoticias(this.categorias[0]);
+  }
+
+  onSegmentChange(e: CustomEvent) {
+    this.noticias = [];
+    this.cargarNoticias(e.detail.value);
+  }
+
+  private cargarNoticias(categoria: string): void {
+
+    this
+      .noticiasService
+      .getTopHeadlinesByCategory(categoria)
+      .pipe(
+        tap((respuesta: RespuestaTopHeadlines) => this.noticias.push(...respuesta.articles))
+      )
+      .subscribe();
+
   }
 
 }
