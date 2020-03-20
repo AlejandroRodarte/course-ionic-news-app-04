1bd6d2b92668cb5d7066e9ac46eddda62b9cebf0
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-19 21:17:08 -0600

    105. Mostrar action-sheet en las noticias

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-noticia',
@@ -16,13 +17,47 @@ export class NoticiaComponent implements OnInit {
   public index: number;
 
   constructor(
-    private inAppBrowser: InAppBrowser
+    private inAppBrowser: InAppBrowser,
+    private actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit() {}
 
   abrirNoticia(): void {
     const browser = this.inAppBrowser.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu(): Promise<void> {
+
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share',
+          handler: () => {
+            console.log('Compartir');
+          }
+        },
+        {
+          text: 'Favorito',
+          icon: 'star',
+          handler: () => {
+            console.log('Favorito');
+          }
+        },
+        {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+
   }
 
 }
