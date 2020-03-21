57fd15d9377ae6d63926d791d981db6aaf63b702
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-21 17:54:19 -0600

    119. Corriendo nuestra PWA en un dispositivo móvil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,14 @@ import { environment } from '../environments/environment';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, IonicStorageModule.forRoot(), ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot(),
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [
     StatusBar,
     SplashScreen,
